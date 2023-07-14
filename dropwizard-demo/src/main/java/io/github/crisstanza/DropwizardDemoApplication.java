5e2bb78059022ae77c3abcad6535ff972cfeccbb
Cris Stanza <crisstanza@users.noreply.github.com>
2023-07-14 15:27:03 -0300

    add healthCheck

--- dropwizard-demo/src/main/java/io/github/crisstanza/DropwizardDemoApplication.java
+++ dropwizard-demo/src/main/java/io/github/crisstanza/DropwizardDemoApplication.java
@@ -3,6 +3,8 @@ package io.github.crisstanza;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
+import io.github.crisstanza.health.TemplateHealthCheck;
+import io.github.crisstanza.resources.HelloWorldResource;
 
 public class DropwizardDemoApplication extends Application<DropwizardDemoConfiguration> {
 
@@ -23,7 +25,17 @@ public class DropwizardDemoApplication extends Application<DropwizardDemoConfigu
     @Override
     public void run(final DropwizardDemoConfiguration configuration,
                     final Environment environment) {
-        // TODO: implement application
-    }
+        // getting-started: HelloWorldApplication#run->HelloWorldResource
+        HelloWorldResource resource = new HelloWorldResource(
+                configuration.getTemplate(),
+                configuration.getDefaultName()
+        );
+        environment.jersey().register(resource);
+        // getting-started: HelloWorldApplication#run->HelloWorldResource
 
+        // getting-started: HelloWorldApplication#run->TemplateHealthCheck
+        TemplateHealthCheck healthCheck = new TemplateHealthCheck(configuration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);
+        // getting-started: HelloWorldApplication#run->TemplateHealthCheck    }
+    }
 }
--- dropwizard-demo/src/main/java/io/github/crisstanza/DropwizardDemoConfiguration.java
+++ dropwizard-demo/src/main/java/io/github/crisstanza/DropwizardDemoConfiguration.java
@@ -3,8 +3,32 @@ package io.github.crisstanza;
 import io.dropwizard.core.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.validator.constraints.*;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotEmpty;
 
 public class DropwizardDemoConfiguration extends Configuration {
-    // TODO: implement service configuration
+    @NotEmpty
+    private String template;
+
+    @NotEmpty
+    private String defaultName = "Stranger";
+
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String name) {
+        this.defaultName = name;
+    }
 }
